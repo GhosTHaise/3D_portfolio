207a0f2a7346ab7d3704dd220b461e3b07eb0e29
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-22 19:48:31 +0300

    feat : add ref

--- src/sections/show-case.tsx
+++ src/sections/show-case.tsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import { useRef } from 'react'
 
 const ShowCase = () => {
-  return (
-    <div id="work" className='app-showcase'>
-        <div className="w-full">
-            <div className="showcaselayout">
-                <div className="first-project-wrapper">
-                    <div className="image-wrapper">
-                        <img src="/images/project1.png" alt="rude" />
+    const sectionRef = useRef<HTMLDivElement>(null);
+    const project1Ref = useRef<HTMLDivElement>(null);
+    const project2Ref = useRef<HTMLDivElement>(null);
+    const project3Ref = useRef<HTMLDivElement>(null);
+    
+    return (
+        <section ref={sectionRef} id="work" className='app-showcase'>
+            <div className="w-full">
+                <div className="showcaselayout">
+                    <div className="first-project-wrapper" ref={project1Ref}>
+                        <div className="image-wrapper">
+                            <img src="/images/project1.png" alt="rude" />
+                        </div>
+                        <div className="text-content">
+                            <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h2>
+                            <p className='text-white-50 md:text-xl'>
+                                An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                            </p>
+                        </div>
                     </div>
-                    <div className="text-content">
-                        <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h2>
-                        <p className='text-white-50 md:text-xl'>
-                            An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
-                        </p>
-                    </div>
-                </div>
 
-                <div className="project-list-wrapper overflow-hidden">
-                    <div className="project">
-                        <div className="image-wrapper bg-[#ffefdb]">
-                            <img src="/images/project2.png" alt="Library Management" />
+                    <div className="project-list-wrapper overflow-hidden">
+                        <div className="project" ref={project2Ref}>
+                            <div className="image-wrapper bg-[#ffefdb]">
+                                <img src="/images/project2.png" alt="Library Management" />
+                            </div>
+                            <h2>Library Management Platform</h2>
                         </div>
-                        <h2>Library Management Platform</h2>
-                    </div>
-                    <div className="project">
-                        <div className="image-wrapper bg-[#ffe7eb]">
-                            <img src="/images/project3.png" alt="YC Directory" />
+                        <div className="project" ref={project3Ref}>
+                            <div className="image-wrapper bg-[#ffe7eb]">
+                                <img src="/images/project3.png" alt="YC Directory" />
+                            </div>
+                            <h2>YC Directory - A Startup Showcase App</h2>
                         </div>
-                        <h2>YC Directory - A Startup Showcase App</h2>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-  )
+        </section>
+    )
 }
 
 export default ShowCase
