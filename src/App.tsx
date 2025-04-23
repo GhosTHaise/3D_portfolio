12d30a1d25be8acab4905253ff6690a391bd1a92
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-23 20:56:20 +0300

    feat : navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/navbar"
 import Hero from "./sections/hero"
 import ShowCase from "./sections/show-case"
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowCase />
     </>
