4daa5fdf0d152d5c93f95fed96748daccf5338bd
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-25 21:13:00 +0300

    feat : experience

--- src/sections/experience.tsx
+++ src/sections/experience.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { expCards } from '../../constants'
+import TitleHeader from '../components/title-header'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id="experience" className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader 
+          title="Professional Work Experience"
+          sub="💼 My Career Overview"
+        />
+
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {
+              expCards.map((card, index) => (
+                <div key={card.title} className='exp-card-wrapper'>
+                  {card.title}
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
