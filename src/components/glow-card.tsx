1e1ca91181e9704fb2497bf4858917770aec83c1
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-26 20:18:50 +0300

    feat : card effect

--- src/components/glow-card.tsx
+++ src/components/glow-card.tsx
@@ -1,8 +1,25 @@
-import { ReactNode } from 'react'
+import { ReactNode, useRef } from 'react'
 
-const GlowCard = ({ card, children } : { card: { title: string, imgPath: string , review : string}, children: ReactNode }) => {
+const GlowCard = ({ card, children, index } : { card: { title: string, imgPath: string , review : string}, children: ReactNode, index : number }) => {
+    const cardRefs = useRef<HTMLDivElement[]>([]);
+
+    const handleMouseMove = (index : number) => (e : MouseEvent) => {
+        const card = cardRefs.current[index];
+        if(!card) return;
+
+        const rect = card.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left - rect.width / 2;
+        const mouseY = e.clientY - rect.top - rect.height / 2;
+
+        let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+        angle = (angle + 360) % 360;
+
+        card.style.setProperty('--start', (angle + 60).toString());
+    }
+
   return (
-    <div className="card card-border timeline-card rounded-xl p-10">
+    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className="card card-border timeline-card rounded-xl p-10">
         <div className="glow" />
         <div className='flex items-center gap-` mb-5'>
             {
