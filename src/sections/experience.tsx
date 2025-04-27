9993595a081113fce47de86768edbc455242494e
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-27 17:38:53 +0300

    feat : animate the experience card

--- src/sections/experience.tsx
+++ src/sections/experience.tsx
@@ -1,8 +1,29 @@
-import { expCards } from '../../constants'
-import GlowCard from '../components/glow-card'
-import TitleHeader from '../components/title-header'
+import { expCards } from '../../constants';
+import GlowCard from '../components/glow-card';
+import TitleHeader from '../components/title-header';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Experience = () => {
+  useGSAP(() => {
+    gsap.utils.toArray('.timeline-card').forEach((card) => {
+      gsap.from(card,{
+        xPercent : -100,
+        opacity : 0,
+        transformOrigin : 'left left',
+        duration : 1,
+        ease : "power1.inOut",
+        scrollTrigger : {
+          trigger : card,
+          start : "top 80%"
+        }
+      })
+    })
+  },[]);
+
   return (
     <section id="experience" className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
       <div className="w-full h-full md:px-20 px-5">
